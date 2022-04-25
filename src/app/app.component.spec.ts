82f40d8b57925289f7328eea6f4bfa7718aca15c
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2022-04-25 19:41:32 +0430

    [feat] add heat map js & implement click tracker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'click-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('click-tracker');
   });
 
   it('should render title', () => {
